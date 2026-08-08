7df1f8edd6dbcdf7383cd2e3d6f9bb06e86f6017
Teboho Masango <blazebeyondreality@gmail.com>
2026-08-09 00:53:48 +0200

    Updated App.js file under src

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
-import React from "react";
-
+import React, { useState } from "react";
+import Header from "./components/Header";
+import CoffeeForm from "./components/CoffeeForm";
+import CoffeeList from "./components/CoffeeList";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Brew Log</div>;
+  let [brews, setBrews] = useState([
+    {
+      id: 1,
+      beanName: "Guatemala Huehuetenango",
+      brewMethod: "V60",
+      coffeeGrams: 18,
+      waterGrams: 300,
+      notes: "Rich and smooth blend.",
+    },
+  ]);
+
+  let handleAddBrew = (newBrew) => {
+    setBrews([newBrew, ...brews]);
+  };
+
+  return (
+    <div
+      style={{
+        maxWidth: "600px",
+        margin: "0 auto",
+        padding: "32px",
+        fontFamily: "sans-serif",
+      }}
+    >
+      <Header />
+      <CoffeeForm onAddBrew={handleAddBrew} />
+      <CoffeeList brews={brews} />
+    </div>
+  );
 }
